3c94ec8a300571c761f427dc7ecd19b059915859
veto <veto@dev.local>
2025-11-25 10:02:21 +0700

    up: po_translator/Makefile po_translator/domain-translate-de_DE.po po_translator/domain-translate-th.po po_translator/file.po po_translator/src/main.rs

--- po_translator/src/main.rs
+++ po_translator/src/main.rs
@@ -1,5 +1,12 @@
 use rspolib::{pofile, prelude::*};
 
 fn main() {
-    println!("Hello, world!");
+    let po = pofile("./domain-translate-de_DE.po").unwrap();
+
+    for entry in &po.entries {
+        println!("{}", entry.msgid);
+        println!("{:?}", entry.msgstr);
+        println!("{:?}", entry.msgstr_plural);
+    }
+    po.save("./file.po");
 }
