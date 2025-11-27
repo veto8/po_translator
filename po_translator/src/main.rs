3cfadc28fefd4d4b9763b2d09a6c016607dd9942
veto <veto@lab.local>
2025-11-27 22:47:12 +0700

    up: po_translator/de.po po_translator/src/main.rs

--- po_translator/src/main.rs
+++ po_translator/src/main.rs
@@ -17,18 +17,27 @@ struct Trans {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let p = "./de.po";
+    let p = "./th.po";
     let mut po = pofile(p).unwrap();
     //let api = "http://127.0.0.1:8089";
     let api = "https://mtranslate.myridia.com";
+    let mut source_lang = "en";
+    let mut target_lang = "en";
 
-    let target_lang = po.metadata["Language"].as_str();
-    let source_lang = "en";
-    //println!("{:?}", source_lang);
+    if po.metadata.contains_key("Language") {
+        target_lang = po.metadata["Language"].as_str();
+    }
+    if po.metadata.contains_key("X-Source-Language") {
+        source_lang = po.metadata["X-Source-Language"].as_str();
+    }
+
+    println!("{:?}", source_lang);
+    println!("{:?}", target_lang);
+
     for entry in &mut po.entries {
         //println!("{}", entry.msgid);
         //        entry.msgstr.replace("".to_string());
-        if !entry.translated() {
+        if !entry.translated() && target_lang != "" {
             if !is_url(&entry.msgid) {
                 println!("{}", entry.msgid);
                 //println!("{:?}", entry.msgstr);
@@ -40,7 +49,7 @@ async fn main() -> Result<(), Error> {
                 //println!("{:?}", url);
 
                 let r = reqwest::get(url).await?.json::<Trans>().await?;
-                //println!("{:?}", &r.target_value);
+                println!("{}", &r.target_value);
                 entry.msgstr.replace(r.target_value.to_string());
             }
         }
